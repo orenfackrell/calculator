c616d3e5191da6279482c72941a095594bd08cc9
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-06-29 15:20:34 +0100

    create 'operate' function

--- calculator.js
+++ calculator.js
@@ -1,15 +1,34 @@
-const add = function(a, b) {
+const add = document.querySelector('.addition');
+const subtract = document.querySelector('.subtract'); 
+const multiply = document.querySelector('.multiply');
+const divide = document.querySelector('.divide'); 
+const clear = document.querySelector('.clear');
+const equals = document.querySelector('.equals'); 
+
+const addition = function(a, b) {
 	return a + b;
 };
 
-const subtract = function(a, b) {
+const subtraction = function(a, b) {
 	return a - b;
 };
 
-const multiply = function(a, b) {
+const multiplication = function(a, b) {
 	return a * b;
 };
 
-const divide = function(a, b) {
+const division = function(a, b) {
 	return a / b;
+};
+
+function operate(operator, a, b){
+    if(operator === add.textContent){
+        addition(a, b);
+    } else if(operator === subtract.textContent){
+        subtract(a, b);
+    } else if(operator == multiply.textContent){
+        multiplication(a, b);
+    } else {
+        division(a, b);
+    }
 };
