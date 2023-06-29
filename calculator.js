2e6696c65a4ac8d8a5831f5150faef6a7f76c92f
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-06-29 16:40:35 +0100

    have inputs show in display and update on equals

--- calculator.js
+++ calculator.js
@@ -21,7 +21,7 @@ const division = function(a, b) {
 	return a / b;
 };
 
-function operate(a,operator, b){
+function operate(a, operator, b){
     if(operator === '+'){
         return addition(a, b);
     } else if(operator === '-'){
@@ -35,7 +35,30 @@ function operate(a,operator, b){
     }
 };
 
-console.log(operate(5, '+', 10))
-console.log(operate(5, '-', 10))
-console.log(operate(5, 'x', 10))
-console.log(operate(5, '÷', 10))
+console.log(operate(5, '+', 10));
+console.log(operate(5, '-', 10));
+console.log(operate(5, 'x', 10));
+console.log(operate(5, '÷', 10));
+
+let buttons = document.querySelectorAll('button')
+let number = document.querySelectorAll('.number');
+let operator =  document.querySelectorAll('.operator');
+let output = document.querySelector('.output');
+let equal = document.querySelector('.equals');
+let previousOutput = document.querySelector('.previousOutput');
+
+number.forEach(button => button.addEventListener('click', () =>{ 
+    let number = button.textContent;
+    output.textContent += `${number}`;
+}));
+
+operator.forEach(button => button.addEventListener('click', () =>{ 
+        let operator = button.textContent;
+        output.textContent += ` ${operator} `;
+}));
+
+
+
+equal.addEventListener('click', () => {
+    previousOutput.textContent = output.textContent;
+});
