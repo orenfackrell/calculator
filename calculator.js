d003bda064f9173481c0980cebcf8b4443223ac0
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-06-29 17:47:29 +0100

    functioning for simple calculations & all buttons

--- calculator.js
+++ calculator.js
@@ -42,23 +42,50 @@ console.log(operate(5, '÷', 10));
 
 let buttons = document.querySelectorAll('button')
 let number = document.querySelectorAll('.number');
-let operator =  document.querySelectorAll('.operator');
+let operators =  document.querySelectorAll('.operator');
 let output = document.querySelector('.output');
-let equal = document.querySelector('.equals');
 let previousOutput = document.querySelector('.previousOutput');
+let backspace = document.querySelector('.backspace');
+
+
+
+function constructCalculation(){
+let calcStorage = [];
+
+clear.addEventListener('click', () =>{
+    output.textContent = null;
+    previousOutput.textContent = null;
+
+})
 
 number.forEach(button => button.addEventListener('click', () =>{ 
     let number = button.textContent;
     output.textContent += `${number}`;
 }));
 
-operator.forEach(button => button.addEventListener('click', () =>{ 
-        let operator = button.textContent;
-        output.textContent += ` ${operator} `;
+operators.forEach(button => button.addEventListener('click', () =>{     
+    let operators = button.textContent;
+    output.textContent += ` ${operators} `;
 }));
 
+backspace.addEventListener('click', () => {
+    output.textContent = output.textContent.slice(0, -1);
+})
 
+equals.addEventListener('click', () => {
 
-equal.addEventListener('click', () => {
-    previousOutput.textContent = output.textContent;
+    previousOutput.textContent = `${output.textContent} =`;
+
+    calcStorage = output.textContent.split(' ');
+
+    console.log(calcStorage)
+
+    let a = parseFloat(calcStorage[0]);
+    let operator = calcStorage[1];
+    let b = parseFloat(calcStorage[2]);
+
+    output.textContent = operate(a, operator, b)
 });
+}
+
+constructCalculation()
