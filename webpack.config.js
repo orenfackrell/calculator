eccedb93ec1a4896d0d403e00d466b78fe436791
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-09-28 12:50:21 +0100

    fix webpack styles, create new module files

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,17 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  watch: true,
-};
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
+},
+watch: true,
+}
