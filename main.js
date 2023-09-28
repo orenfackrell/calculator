2113914c447b03efdd4eab46b7800d19411f755d
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-09-28 12:50:21 +0100

    fix webpack styles, create new module files

--- main.js
+++ main.js
@@ -21,12 +21,12 @@ eval("\n\n//# sourceURL=webpack://calculator/./src/index.js?");
 
 /******/ 	});
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	
+/******/
 /******/ })()
 ;
